f30d1f05b7e2e3528e0ef4f85c7e980fd02de228
MimixSoftware <mimixsoftware@gmail.com>
2024-12-05 17:44:51 +0000

    Fixed search bug

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,38 +1,63 @@
 const Fuse = require('fuse.js')
 
 // todo will make this information be read from the database but will let it be hardcoded for now 
+// DONE!
+// const clubs = [
+//     {
+//         name: "MTU United",
+//         sport: "Football"
+//     },
+//     {
+//         name: "MTU GAA",
+//         sport: "GAA"
+//     },
+//     {
+//         name: "MTU RFC",
+//         sport: "Rugby"
+//     },
+//     {
+//         name: "Leisure World Swimming",
+//         sport: "Swimming"
+//     },
+//     {
+//         name: "MTU Arena Gym",
+//         sport: "Weightlifting"
+//     },
+//     {
+//         name: "MTU Athletics",
+//         sport: "Running"
+//     }
+// ];
 
-const clubs = [
-    {
-        name: "MTU United",
-        sport: "Football"
-    },
-    {
-        name: "MTU GAA",
-        sport: "GAA"
-    },
-    {
-        name: "MTU RFC",
-        sport: "Rugby"
-    },
-    {
-        name: "Leisure World Swimming",
-        sport: "Swimming"
-    },
-    {
-        name: "MTU Arena Gym",
-        sport: "Weightlifting"
-    },
-    {
-        name: "MTU Athletics",
-        sport: "Running"
-    }
-];
+let clubs = [];
 
 const fuse = new Fuse(clubs, {
     keys: ['name', 'sport'],
     threshold: 0.3
 });
+
+async function loadClubs() {
+    const response = await fetch('https://showallclubs-77hki32qna-nw.a.run.app', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        clubs = Object.keys(data.clubs).map(clubKey => ({
+            name: clubKey,
+            sport: data.clubs[clubKey].sport
+        }));
+        console.log('Transformed Clubs:', clubs);
+        console.log(fuse.list);
+        fuse.setCollection(clubs);
+    } else {
+        console.error("Failed to load clubs");
+    }
+}
+loadClubs();
 
 const userInput = document.getElementById('userInput');
 const resultsDiv = document.getElementById('results');

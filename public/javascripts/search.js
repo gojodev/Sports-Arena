8f64aa1b33faaea573e23f984dfe390b7b7e20f9
MimixSoftware <mimixsoftware@gmail.com>
2024-12-08 09:43:02 +0000

    in search all clubs are displayed initially before any input

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,38 +1,9 @@
-const Fuse = require('fuse.js')
-
-// todo will make this information be read from the database but will let it be hardcoded for now 
-// DONE!
-// const clubs = [
-//     {
-//         name: "MTU United",
-//         sport: "Football"
-//     },
-//     {
-//         name: "MTU GAA",
-//         sport: "GAA"
-//     },
-//     {
-//         name: "MTU RFC",
-//         sport: "Rugby"
-//     },
-//     {
-//         name: "Leisure World Swimming",
-//         sport: "Swimming"
-//     },
-//     {
-//         name: "MTU Arena Gym",
-//         sport: "Weightlifting"
-//     },
-//     {
-//         name: "MTU Athletics",
-//         sport: "Running"
-//     }
-// ];
+const Fuse = require('fuse.js');
 
 let clubs = [];
 
 const fuse = new Fuse(clubs, {
-    keys: ['name', 'sport'],
+    keys: ['name', 'sport', 'trainer'],
     threshold: 0.3
 });
 
@@ -72,12 +43,30 @@ async function loadClubs() {
         }));
         
         console.log('Transformed Clubs:', clubs);
-        console.log(fuse.list);
         fuse.setCollection(clubs);
+
+        displayClubs(clubs);
     } else {
         console.error("Failed to load clubs");
     }
 }
+
+function displayClubs(clubsToDisplay) {
+    resultsDiv.innerHTML = '';
+
+    if (clubsToDisplay.length === 0) {
+        resultsDiv.textContent = "No clubs found.";
+        return;
+    }
+
+    clubsToDisplay.forEach(club => {
+        const div = document.createElement('div');
+        div.classList.add('result');
+        div.textContent = `${club.name} \nSport: ${club.sport} \nTrainer: ${club.trainer} \nCalorie Burn Rate: ${club.caloryBurnRate}cal/hr`;
+        resultsDiv.appendChild(div);
+    });
+}
+
 loadClubs();
 
 const userInput = document.getElementById('userInput');
@@ -85,21 +74,12 @@ const resultsDiv = document.getElementById('results');
 
 userInput.addEventListener('input', () => {
     const query = userInput.value;
-    const results = fuse.search(query);
 
-    //clear prev results
-    resultsDiv.innerHTML = '';
-
-    //display new results
-    results.forEach(result => {
-        const club = result.item;
-        const div = document.createElement('div');
-        div.classList.add('result');
-        div.textContent = `${club.name} \nSport: ${club.sport} \nTrainer: ${club.trainer} \nCalorie Burn Rate: ${club.caloryBurnRate}cal/hr`;
-        resultsDiv.appendChild(div);
-    });
-
-    if (results.length === 0) {
-        resultsDiv.textContent = "No clubs found.";
+    if (!query) {
+        displayClubs(clubs);
+    } 
+    else {
+        const results = fuse.search(query);
+        displayClubs(results.map(result => result.item));
     }
 });
